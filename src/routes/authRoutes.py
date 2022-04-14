9384818cb624b499237b1eec21ca4fe64ad04e8e
Joao Marconi Dos Santos Filho <joao.marconi@linx.com.br>
2022-04-14 15:36:53 -0300

    commit

--- src/routes/authRoutes.py
+++ src/routes/authRoutes.py
@@ -4,7 +4,7 @@ import datetime
 
 from views.authForms import FormLogin
 from controllers.authController import authLogin
-from models.models import ANALISTA, BASES, GESTOR, HELPER, CONTROLE_TPS_GERAIS, CONTROLE_TPS_ANALISTAS
+from models.models import ANALISTA, BASES, GESTOR, HELPER, CONTROLE_TPS_GERAIS, CONTROLE_TPS_ANALISTAS, TIME
 
 def init_app(app: Flask):
     @app.route("/", methods=['GET', 'POST'])
@@ -26,9 +26,21 @@ def init_app(app: Flask):
         bases_oracle = BASES.query.filter_by(ESTRUTURA='Oracle').all()
         bases_sqlserver = BASES.query.filter_by(ESTRUTURA='SQLServer').all()
         if GESTOR.query.filter_by(EMAIL=current_user.EMAIL).first():
-            return render_template('/main/dashboards/dashboardGestor.html', bases_oracle=bases_oracle, bases_sqlserver=bases_sqlserver, tps_gerais=tps_gerais)
+            time = TIME.query.filter_by(GESTOR_ID=current_user.id).first()
+            analistas_gestor = []
+            for analista in time.ANALISTAS:
+                analistas_gestor.append(analista.USUARIO)
+
+            tps_time = CONTROLE_TPS_ANALISTAS.query.filter(ANALISTA.USUARIO.in_(analistas_gestor)).all()
+            return render_template('/main/dashboards/dashboardGestor.html', bases_oracle=bases_oracle, bases_sqlserver=bases_sqlserver, tps_gerais=tps_gerais, tps_time=tps_time, data_hoje=data_hoje)
         elif HELPER.query.filter_by(EMAIL=current_user.EMAIL).first():
-            return render_template('/main/dashboards/dashboardHelper.html', bases_oracle=bases_oracle, bases_sqlserver=bases_sqlserver, tps_gerais=tps_gerais)
+            time = TIME.query.filter_by(HELPER_ID=current_user.id).first()
+            analistas_help = []
+            for analista in time.ANALISTAS:
+                analistas_help.append(analista.USUARIO)
+
+            tps_time = CONTROLE_TPS_ANALISTAS.query.filter(ANALISTA.USUARIO.in_(analistas_help)).all()
+            return render_template('/main/dashboards/dashboardHelper.html', bases_oracle=bases_oracle, bases_sqlserver=bases_sqlserver, tps_gerais=tps_gerais, tps_time=tps_time, data_hoje=data_hoje)
         else:
             return render_template('/main/dashboards/dashboardAnalista.html', bases_oracle=bases_oracle, bases_sqlserver=bases_sqlserver, tps_gerais=tps_gerais, tps_analistas=tps_analistas, data_hoje=data_hoje)
 
